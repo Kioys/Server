a1c4ed30476cd8a4f252805275573502df342753
Kioys <44926624+Kioys@users.noreply.github.com>
2018-12-05 20:27:13 -0300

    OK-working server
    I finally fix a bug that crashes everything, this is a big step for the Server side of the chat, it is not finished but like I said this is a big step, I have a lot of improvements in mind.

---
+++ Client.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Net.Sockets;
+using System;
+
+namespace Server
+{
+    //la clase Client maneja un socket por separado, asi se pueden manejar multiples clientes al mismo tiempo sin generar errores
+    //cada Client manejara un metodo asyncronico que se llama readCallback, (llamar Client.readCallback al usar metodo BeginReceive)
+    public class Client
+    {
+        public Socket socket = null;
+        public const int BufferSize = 1024;
+        public byte[] buffer = new byte[BufferSize];
+        string[] fMsg;
+
+        public Client(Socket listener)
+        {
+            socket = listener;
+        }
+
+        public void ReadCallback(IAsyncResult ar)
+        {
+            Socket so = (Socket)ar.AsyncState;
+            Socket handler = so;
+
+            try
+            {
+                int read = handler.EndReceive(ar);
+                if (read > 0)
+                {
+                    fMsg = Encoding.ASCII.GetString(buffer, 0, read).Split('~');
+                    if (read > 0)
+                    {
+                        if (fMsg[2] != "PARAM_EXIT")
+                        {
+                            Console.WriteLine("[{1}] MESSAGE -> {0}", fMsg[1], handler.RemoteEndPoint);
+                            Program.SendMsg(handler, buffer);
+                            handler.BeginReceive(buffer, 0, BufferSize, 0, new AsyncCallback(ReadCallback), handler);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[{1}] DISCONNECTED -> {0}", fMsg[1], handler.RemoteEndPoint);
+                            Program.SendMsg(handler, buffer);
+                            handler.Disconnect(true);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                handler.Disconnect(true);
+                Program.Clients.Remove(this);
+            }
+        }
+    }
+}
